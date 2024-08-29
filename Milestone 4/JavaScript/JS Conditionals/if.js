67a03e29dd9ed10eab9730e7c85c248b77911ed4
ashraful2871 <ashrafulislam2758@gmail.com>
2024-08-29 22:09:02 +0600

    add another file

---
+++ Milestone 4/JavaScript/JS Conditionals/if.js
@@ -0,0 +1,17 @@
+if (13 < 10) {
+  console.log("i have a smaller value");
+}
+
+var birianyPrice = 250;
+
+if (birianyPrice < 300) {
+  console.log("mama give me a some biriyany");
+}
+
+var weight = 20;
+
+if (weight <= 20) {
+  console.log("i will carry it by myself");
+} else {
+  console.log("i will rent a rikshaw");
+}
---
+++ Milestone 4/JavaScript/JS Conditionals/resturent.js
@@ -0,0 +1,25 @@
+// const age = 12;
+// const price = 500;
+
+// if (age < 12) {
+//   console.log("you can eat free");
+// } else if (age >= 60) {
+//   //50% discount
+//   const discount = (price * 50) / 100;
+//   const payAmount = price - discount;
+//   console.log(payAmount);
+// } else if (age >= 50) {
+// } else {
+//   console.log(price);
+// }
+
+// let result = 10 < "5";
+// console.log(result);
+
+let x = 5;
+if (x < 10) {
+  x = x + 3;
+} else {
+  x = x - 2;
+}
+console.log(x);
